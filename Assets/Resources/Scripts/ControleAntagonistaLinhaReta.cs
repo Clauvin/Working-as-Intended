869e129c04dac64b258cabd4f21e3bf3ea1538d8
Clauvin <clauvin_almeida@hotmail.com>
2016-12-26 16:08:37 -0200

    Projéteis desaparecem por tempo

--- Assets/Resources/Scripts/ControleAntagonistaLinhaReta.cs
+++ Assets/Resources/Scripts/ControleAntagonistaLinhaReta.cs
@@ -8,6 +8,9 @@ public class ControleAntagonistaLinhaReta : MonoBehaviour {
     public float accmody = 0.00f;
     public float velx = 0.0f;
     public float vely = 0.0f;
+    public float distancia_a_ser_percorrida_para_desaparecer = 100.0f;
+    public float distancia_percorrida = 0.0f;
+
     private float posx = 0.0f;
     private float posy = 0.0f;
 
@@ -42,10 +45,15 @@ public class ControleAntagonistaLinhaReta : MonoBehaviour {
         posx += velx;
         posy += vely;
 
+        //Calcula a distancia percorrida pelo projétil
+        distancia_percorrida += Mathf.Sqrt((trab.x - posx) * (trab.x - posx) + (trab.y - posy) * (trab.y - posy));
+
         //Altera valor de X e Y da posição.
         trab.x = posx;
         trab.y = posy;
         GetComponent<Transform>().position = trab;
+
+        if (distancia_percorrida >= distancia_a_ser_percorrida_para_desaparecer) Destroy(gameObject);
 
     }
 }
