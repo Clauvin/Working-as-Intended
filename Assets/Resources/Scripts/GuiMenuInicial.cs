22c2bf4251728bdc1d5b909dcbffa1ab51d8c3c9
Clauvin <clauvin_almeida@hotmail.com>
2016-12-27 15:45:44 -0200

    Menu Inicial funcionando como deve

--- Assets/Resources/Scripts/GuiMenuInicial.cs
+++ Assets/Resources/Scripts/GuiMenuInicial.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using Basicas_1;
 
 /// <summary>
@@ -15,7 +13,7 @@ public class GuiMenuInicial : GuiPadrao2 {
     private int posicaox;
     private int qualbotao = -1;
     private int resultado = -1;
-    private string[] toolbarStrings = { "Visualizar Log F!T", "Créditos", "Sair" };
+    private string[] toolbarStrings = { "Iniciar Jogo", "Créditos", "Sair" };
 
     private bool creditos = false;
 
@@ -43,7 +41,7 @@ public class GuiMenuInicial : GuiPadrao2 {
         posicaox = 0;
         GUI.Box(new Rect(0, 0, Screen.width, Screen.height), string.Empty);
         GUI.Box(new Rect(Screen.width / 4, Screen.height / 6, Screen.width / 2, Screen.height / 2),
-            "Visualizador de Logs\n\n" + "F!T\n\n" + "1.0.1-beta", estilotitulotelainicial);
+            "Jogo 000 - Working as \n" + "Intended", estilotitulotelainicial);
 
         resultado = GUI.Toolbar(new Rect(Screen.width / 12 * 3, Screen.height / 10 * 8,
                                         Screen.width / 12 * 6, Screen.height / 10), qualbotao,
@@ -77,9 +75,7 @@ public class GuiMenuInicial : GuiPadrao2 {
 
             GUI.Box(new Rect(0, 0, Screen.width * 0.2f, Screen.height * 0.3f), "Créditos\n\n" +
                 "Cláuvin Erlan José\n da Costa Curty de Almeida\n" +
-                "(clauvin_almeida@hotmail.com)\n" +
-                "Contribuidor: Bruno Correia\n" +
-                "(acrescentar e-mail de Bruno Correia).");
+                "(clauvin_almeida@hotmail.com)");
 
             if (GUI.Button(new Rect(Screen.width * 0.05f, Screen.height * 0.25f, Screen.width * 0.1f, Screen.height * 0.05f),
                 "Fechar"))
