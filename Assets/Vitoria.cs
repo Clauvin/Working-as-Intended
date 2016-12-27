2c69ecdc5fcab779e789d81d672cb66394f4b961
Clauvin <clauvin_almeida@hotmail.com>
2016-12-27 16:54:44 -0200

    Tela de Vitória integrada ao jogo

---
+++ Assets/Vitoria.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Basicas_1;
+
+public class Vitoria : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (GetComponent<Timer>().tempo <= 64) CarregaCena.Carrega((int)Cenas.Victory_Screen);
+	}
+}
