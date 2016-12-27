94d86de6086a3788a4cbc97d8c5acaf6461c63a6
Clauvin <clauvin_almeida@hotmail.com>
2016-12-27 16:32:41 -0200

    Atualizando o Texto do timer que nao funciona ainda

---
+++ Assets/Resources/Scripts/AtualizandoTextoDoTimer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AtualizandoTextoDoTimer : MonoBehaviour {
+
+    private string tempo;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        tempo = (GetComponent<Timer>().tempo - 65).ToString();
+        GetComponent<Text>().text = tempo;
+        
+    }
+}
