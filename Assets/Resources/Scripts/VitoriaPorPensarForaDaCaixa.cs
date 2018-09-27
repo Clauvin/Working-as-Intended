06b9c154b9eee3471f2acdec56a15268a03fe197
Clauvin <clauvin_almeida@hotmail.com>
2018-09-27 11:40:26 -0300

    (#5) Criado script VitoriaPorPensarForaDaCaixa

---
+++ Assets/Resources/Scripts/VitoriaPorPensarForaDaCaixa.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Basicas_1;
+
+public class VitoriaPorPensarForaDaCaixa : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        CarregaCena.Carrega((int)Cenas.Victory_For_Lateral_Thinking);
+    }
+
+}
