3417fdea57f6f624a43b528561aeb9def4ed4d0c
Clauvin <clauvin_almeida@hotmail.com>
2018-09-27 10:59:55 -0300

    (#2) Criada classe ForcaNoProtagonistaEmCasoDeColisao

---
+++ Assets/Resources/Scripts/ForcaNoProtagonistaEmCasoDeColisao.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForcaNoProtagonistaEmCasoDeColisao : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            ControleDoPersonagem controle_do_personagem = other.GetComponent<ControleDoPersonagem>();
+            if (controle_do_personagem == null) Debug.Log("Deu bug aqui.");
+
+            controle_do_personagem.accmodx = controle_do_personagem.accmody = 0;
+            controle_do_personagem.velx *= -1;
+            controle_do_personagem.vely *= -1;
+        }
+    }
+}
