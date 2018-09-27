cbdd176e486a6f57e916ffb694a00063c20b2305
Clauvin <clauvin_almeida@hotmail.com>
2018-09-27 16:43:43 -0300

    (#7) Criador de colisões pronto

--- Assets/Resources/Scripts/AtualizandoTextoDoContadorDeColisoes.cs
+++ Assets/Resources/Scripts/AtualizandoTextoDoContadorDeColisoes.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class AtualizandoTempoDoContadorDeColisoes : MonoBehaviour {
+public class AtualizandoTextoDoContadorDeColisoes : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    private string colisoes;
+    public string texto_inicial = "Colisões = ";
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        colisoes = texto_inicial + FindObjectOfType<ControleDoPersonagem>().contador_de_acertos_recebidos;
+        GetComponent<Text>().text = colisoes;
+
+    }
 }
