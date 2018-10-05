a10b5a153a68f9c3834ca28059c5fbfee8c1465a
Clauvin <clauvin_almeida@hotmail.com>
2018-10-04 22:05:52 -0300

    Canhão roda e se move

---
+++ Assets/ScriptRotationsInSpace.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptRotationsInSpace : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Rotation(1f);
+	}
+
+    public void Rotation(float change_in_angle)
+    {
+        GetComponent<RectTransform>().Rotate(new Vector3(0.0f, 0.0f, change_in_angle));
+    }
+}
---
+++ Assets/ScriptTranslationsInSpace.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptTranslationsInSpace : MonoBehaviour {
+
+    public float left_x_limit = -28.0f;
+    public float right_x_limit = 28.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Translation(-1f, 0, 0);
+	}
+
+    public void Translation(float change_in_x, float change_in_y, float change_in_z)
+    {
+        Vector3 old_position = GetComponent<RectTransform>().position;
+        Vector3 new_position = old_position + new Vector3(change_in_x, change_in_y, change_in_z);
+
+        if (new_position.x < left_x_limit) new_position.x = left_x_limit;
+        if (new_position.x > right_x_limit) new_position.x = right_x_limit;
+
+        GetComponent<RectTransform>().position = new_position;
+    }
+}
