bc2765137f5696307dcbdc8a9ae5a2cd56a3e86a
Clauvin <clauvin_almeida@hotmail.com>
2018-10-04 23:03:10 -0300

    Sensor funciona

---
+++ Assets/ScriptSensesProtagonist.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptSensesProtagonist : MonoBehaviour {
+
+    public bool senses_protagonist = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player") senses_protagonist = true;
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Player") senses_protagonist = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player") senses_protagonist = false;
+    }
+}
