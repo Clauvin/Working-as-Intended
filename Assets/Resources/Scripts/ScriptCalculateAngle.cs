72d289757ce35c10924c6ca213577a0af594de9d
Clauvin <clauvin_almeida@hotmail.com>
2018-10-28 18:52:04 -0300

    Created ScriptCalculateAngle

---
+++ Assets/Resources/Scripts/ScriptCalculateAngle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+public class ScriptCalculateAngle : MonoBehaviour {
+
+    GameObject atirador;
+    GameObject protagonista;
+
+    Vector2 atirador_position;
+    Vector2 protagonista_position;
+    Vector2 third_point_position;
+
+    public float angle;
+
+	// Use this for initialization
+	void Start () {
+        atirador = GameObject.FindGameObjectWithTag("Atirador Móvel");
+        protagonista = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        atirador_position.x = atirador.transform.position.x;
+        atirador_position.y = atirador.transform.position.y;
+
+        protagonista_position.x = protagonista.transform.position.x;
+        protagonista_position.y = protagonista.transform.position.y;
+
+        third_point_position.x = atirador_position.x;
+        third_point_position.y = protagonista_position.y;
+
+        angle = Vector2.SignedAngle(third_point_position, protagonista_position);
+
+    }
+}
